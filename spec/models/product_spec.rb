cb2397f0fd3a74c16ac24ac03f39a0cf6daf53ef
chrynuik <catherine.hrynuik@gmail.com>
2017-12-11 20:46:02 +0000

    added error message checking

--- spec/models/product_spec.rb
+++ spec/models/product_spec.rb
@@ -11,22 +11,26 @@ RSpec.describe Product, type: :model do
       category = Category.create!(name: 'Clothes')
       product = Product.new(price: 20, quantity: 5, category: category)
       expect(product).to_not be_valid
+      expect(product.errors.full_messages).to include('Name can\'t be blank')
 
     end
     it 'should not be valid without a price' do
       category = Category.create!(name: 'Clothes')
       product = Product.new(name: 'Fancy coat', quantity: 5, category: category)
       expect(product).to_not be_valid
+      expect(product.errors.full_messages).to include('Price can\'t be blank')
 
     end
     it 'should not be valid without a quantity' do
       category = Category.create!(name: 'Clothes')
       product = Product.new(name: 'Fancy coat', price: 20, category: category)
       expect(product).to_not be_valid
+      expect(product.errors.full_messages).to include('Quantity can\'t be blank')
     end
     it 'should not be valid without a category' do
       product = Product.new(name: 'Fancy coat', quantity: 5, price: 20)
       expect(product).to_not be_valid
+      expect(product.errors.full_messages).to include('Category can\'t be blank')
     end
   end
 end
